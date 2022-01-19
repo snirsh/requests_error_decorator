0678297b0a92960c245d3faf0a82d8a40bafc81a
snirsh <s.sharristh@gmail.com>
2022-01-19 23:12:01 +0200

    created example and changed the name of the containing py file

---
+++ src/requests_error_handling_package/example.py
@@ -0,0 +1,10 @@
+import requests
+
+from src.requests_error_handling_package.requests_error_decorators import requests_error_handler
+
+
+@requests_error_handler
+def example():
+    response = requests.get("http://google.com/doesntexist")
+    response.raise_for_status()
+    return response.status_code
